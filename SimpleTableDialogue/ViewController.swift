ae0a1b95f191faf34d600ab2dade53bf98dcaf30
Rattee <Rattee_War@truecorp.co.th>
2017-02-06 15:09:34 +0700

    Initial Commit

---
+++ SimpleTableDialogue/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleTableDialogue
+//
+//  Created by Rattee Warriyawhutthiwat on 2/6/2560 BE.
+//  Copyright © 2560 truecorp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
